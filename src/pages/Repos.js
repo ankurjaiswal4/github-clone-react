c6c910761a2b3f539f3b3910fe10b1f72738be7e
Ankur Jaiswal <ankurjaiswalcse@gmail.com>
2019-12-27 15:20:07 +0530

    added search

--- src/pages/Repos.js
+++ src/pages/Repos.js
@@ -3,14 +3,45 @@ import './Repos.css';
 
 const Repos = (props) => {
     const [fullDetails, setFullDetails] = useState([]);
+    const [fullDetailsFetched, setFullDetailsFetched] = useState([]);
 
-    fetch('https://api.github.com/users/supreetsingh247/repos').then(response => response.json()).then(json => setFullDetails(json));
+    const handleChange = (e) => {
+        let text = e.target.value;
+        let res = fullDetailsFetched.filter(function (item) {
+            if (item.name.toLowerCase().includes(text)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
+        if (text == "") {
+            setFullDetails(fullDetailsFetched);
+        }
+        else {
+            setFullDetails(res);
+        }
+        // console.log(res);
+    }
 
+    function fetchRecords() {
+        fetch('https://api.github.com/users/supreetsingh247/repos')
+            .then(response => response.json())
+            .then(json => {
+                setFullDetails(json);
+                setFullDetailsFetched(json);
+            });
+    }
+
+    if (fullDetails.length < 1) {
+        fetchRecords();
+    }
     return (
         <div>
+            <input type="text" placeholder="Enter the repo name" onChange={handleChange}></input>
             {
                 fullDetails.map((repo) =>
-                    <div className="repo-item">
+                    <div className="repo-item" key={repo.id}>
                         <div className="name">
                             <a href={repo.html_url}>{repo.name}</a>
                         </div>
